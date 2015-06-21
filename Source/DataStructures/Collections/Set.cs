d1c2aa755e269e5bbc13f3e0237878a1aae200ce
nKast <nkastellanos@gmail.com>
2015-06-21 20:22:19 +0300

    Add tainicom.DataStructures.Collections.Set<T>

---
+++ Source/DataStructures/Collections/Set.cs
@@ -0,0 +1,75 @@
+﻿#region License
+//   Copyright 2015 Kastellanos Nikolaos
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#endregion
+
+using System.Collections.Generic;
+
+namespace tainicom.DataStructures.Collections
+{
+    public class Set<T>:ICollection<T>
+    {
+        private Dictionary<T, short> _dictionary;
+
+        public Set()
+        {
+            _dictionary = new Dictionary<T,short>();
+        }
+
+        public void Add(T item)
+        {
+            _dictionary.Add(item,0);
+        }
+
+        public void Clear()
+        {
+            _dictionary.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return _dictionary.ContainsKey(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _dictionary.Keys.CopyTo(array,arrayIndex);
+        }
+
+        public int Count
+        {
+            get { return _dictionary.Keys.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(T item)
+        {
+            return _dictionary.Remove(item);
+        }
+        
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _dictionary.Keys.GetEnumerator();
+        }     
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _dictionary.Keys.GetEnumerator();
+        }
+    }
+}
